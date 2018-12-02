1105d43ad30c1d9519851cc6177b342a459e5684
hyojin <ju930611@naver.com>
2018-12-02 14:31:55 +0000

    graduate, faculty class 추가

--- library.h
+++ library.h
@@ -11,13 +11,17 @@ class library{
 		vector <undergraduated> mem_list;
 		vector <seat> seat_list;
 		vector <studyroom> studyroom_list;
+		vector <graduated> grad_list;
+		vector <faculties> fac_list;
 	public:
 		library();
 		void input_get();
 		void data_get();
+		int find_num(string s);
 		int process_s(string s1,string s2,string s3,string s4,string s5,string s6,string s7,string s8);
 		int process_r(string s1, string s2, string s3, string s4, string s5, string s6);
-		void output(int i);
+		void output_r(int i);
+		void output_s(int i);
 		string inttoday_r(int i);
 		int daytoint_r(string s);
 		int daytoint_s(string s);
